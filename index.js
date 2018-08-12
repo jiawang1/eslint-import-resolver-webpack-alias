f51e4960465edc22171d63d509f95273286c27ce
jay.wang <jay.wang@ef.com>
2018-08-12 21:46:32 +0800

    update version

--- index.js
+++ index.js
@@ -3,47 +3,47 @@
 const path = require('path');
 
 const localResolve = module.constructor._findPath;
-const defaultExtension = ['.js', '.json'];
+const defaultExtension = ['.js', 'jsx', '.json'];
 
 function findConfiguredPath(modulePath, alias) {
-  const __pathes = modulePath.split('/');
-  let aliasPath = null;
+	const __pathes = modulePath.split('/');
+	let aliasPath = null;
 
-  if (__pathes.length === 1) {
-    aliasPath = alias[__pathes[0]] || alias[__pathes[0] + '$'];
-  } else {
-    aliasPath = alias[__pathes[0]];
-  }
-  return aliasPath;
+	if (__pathes.length === 1) {
+		aliasPath = alias[__pathes[0]] || alias[__pathes[0] + '$'];
+	} else {
+		aliasPath = alias[__pathes[0]];
+	}
+	return aliasPath;
 }
 
-function isToFile(path, extensions) {
-  return extensions.some(ext => path.endsWith(ext));
+function fileWithExtension(path, extensions) {
+	return extensions.some(ext => path.endsWith(ext));
 }
 exports.interfaceVersion = 2;
 
 exports.resolve = function(modulePath, sourceFile, config) {
-  if (config && config.resolve && config.resolve.alias) {
-    const __pathes = modulePath.split('/');
+	if (config && config.resolve && config.resolve.alias) {
+		const __pathes = modulePath.split('/');
 
-    const aliasPath = findConfiguredPath(modulePath, config.resolve.alias);
+		const aliasPath = findConfiguredPath(modulePath, config.resolve.alias);
 
-    if (aliasPath) {
-      let filename = null;
+		if (aliasPath) {
+			let filename = null;
 
-      if (isToFile(aliasPath, defaultExtension)) {
-        filename = localResolve(aliasPath, []);
-      } else {
-        const filePath = aliasPath.startsWith('.')
-          ? path.join(__dirname, aliasPath, ...__pathes.slice(1))
-          : path.join(aliasPath, ...__pathes.slice(1));
-        filename = localResolve(filePath, []);
-      }
-      return {
-        found: !!filename,
-        path: filename || null
-      };
-    }
-  }
-  return { found: false };
+			if (fileWithExtension(aliasPath, defaultExtension)) {
+				filename = localResolve(aliasPath, []);
+			} else {
+				const filePath = aliasPath.startsWith('.')
+					? path.join(__dirname, aliasPath, ...__pathes.slice(1))
+					: path.join(aliasPath, ...__pathes.slice(1));
+				filename = localResolve(filePath, []);
+			}
+			return {
+				found: !!filename,
+				path: filename || null,
+			};
+		}
+	}
+	return { found: false };
 };
